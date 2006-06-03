0d71107b5ee893dc334c107a714306509ab2ea7f
Dominik Seichter <domseichter@web.de>
2006-06-03 21:34:35 +0000

    Added FilterTest
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@11 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

---
+++ test/FilterTest/FilterTest.cpp
@@ -0,0 +1,134 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Dominik Seichter                                *
+ *   domseichter@web.de                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "PdfFilter.h"
+
+#include "../PdfTest.h"
+
+using namespace PoDoFo;
+
+/*
+const char pTestBuffer[]  = { 
+    0x01, 0x64, 0x65, 0xFE, 0x6B, 0x80, 0x45, 0x32, 0x88, 0x12, 0x71, 0xEA, 0x01,
+    0x01, 0x64, 0x65, 0xFE, 0x6B, 0x80, 0x45, 0x32, 0x88, 0x12, 0x71, 0xEA, 0x03,
+    0x01, 0x64, 0x65, 0xFE, 0x6B, 0x80, 0x45, 0x32, 0x88, 0x12, 0x71, 0xEA, 0x02,
+    0x01, 0x64, 0x65, 0xFE, 0x6B, 0x80, 0x45, 0x32, 0x88, 0x12, 0x71, 0xEA, 0x00,
+    0x01, 0x64, 0x65, 0xFE, 0x6B, 0x80, 0x45, 0x32, 0x88, 0x12, 0x71, 0xEA, 0x00 
+};
+*/
+
+const char pTestBuffer[]  = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
+
+// const long lTestLength = 5*13;
+#define lTestLength strlen( pTestBuffer )
+
+PdfError test_filter( EPdfFilter eFilter ) 
+{
+    PdfError   eCode;
+    PdfFilter* pFilter;
+
+    char*      pEncoded;
+    char*      pDecoded;
+    long       lEncoded;
+    long       lDecoded;
+   
+    pFilter = PdfFilterFactory::Create( eFilter );
+    if( !pFilter )
+    {
+        printf("!!! Filter %i not implemented.\n", (int)eFilter);
+        return eCode;
+    }
+
+    printf("Testing Algorithm %i:\n", (int)eFilter);
+    printf("\t-> Testing Encoding\n");
+    eCode = pFilter->Encode( pTestBuffer, lTestLength, &pEncoded, &lEncoded );
+    if( eCode == ePdfError_UnsupportedFilter ) 
+    {
+        printf("\t-> Encoding not supported for filter %i.\n", (int)eFilter );
+        return ePdfError_ErrOk;
+    }
+    else if( eCode.IsError() )
+        return eCode;
+
+    printf("\t-> Testing Decoding\n");
+    eCode = pFilter->Decode( pEncoded, lEncoded, &pDecoded, &lDecoded );
+    if( eCode == ePdfError_UnsupportedFilter ) 
+    {
+        printf("\t-> Decoding not supported for filter %i.\n", (int)eFilter);
+        return ePdfError_ErrOk;
+    }
+    else if( eCode.IsError() )
+        return eCode;
+
+    printf("\t-> Original Data Length: %li\n", lTestLength );
+    printf("\t-> Encoded  Data Length: %li\n", lEncoded );
+    printf("\t-> Decoded  Data Length: %li\n", lDecoded );
+
+    if( lTestLength != lDecoded ) 
+    {
+        fprintf( stderr, "Error: Decoded Length != Original Length\n");
+        RAISE_ERROR( ePdfError_TestFailed );
+    }
+
+    if( memcmp( pTestBuffer, pDecoded, lTestLength ) != 0 )
+    {
+        printf("\t-> Original Data: <%s>\n", pTestBuffer );
+        printf("\t-> Encoded  Data: <%s>\n", pEncoded );
+        printf("\t-> Decoded  Data: <%s>\n", pDecoded );
+
+        fprintf( stderr, "Error: Decoded Data does not match original data.\n");
+        RAISE_ERROR( ePdfError_TestFailed );
+    }
+
+    printf("\t-> Test succeeded!\n");
+
+    return eCode;
+}
+
+
+int main( int argc, char* argv[] ) 
+{
+    PdfError        eCode;
+    int             i;
+
+    printf("This test tests all filters of PoDoFo\n");
+    printf("---\n");
+
+    printf("ePdfFilter_ASCIIHexDecode     = 0\n");
+    printf("ePdfFilter_ASCII85Decode      = 1\n");
+    printf("ePdfFilter_LZWDecode          = 2\n");
+    printf("ePdfFilter_FlateDecode        = 3\n");
+    printf("ePdfFilter_RunLengthDecode    = 4\n");
+    printf("ePdfFilter_CCITTFaxDecode     = 5\n");
+    printf("ePdfFilter_JBIG2Decode        = 6\n");
+    printf("ePdfFilter_DCTDecode          = 7\n");
+    printf("ePdfFilter_JPXDecode          = 8\n");
+    printf("ePdfFilter_Crypt              = 9\n");
+
+    for( i=0;i<=ePdfFilter_Crypt && !eCode.IsError();i++ )
+    {
+        eCode = test_filter( (EPdfFilter)i );
+    }
+
+    if( eCode.IsError() )
+        eCode.PrintErrorMsg();
+
+    return eCode.Error();
+}
