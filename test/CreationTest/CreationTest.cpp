4ffd2cfe89a7a46e6b155e9290a6cae967f77c27
Dominik Seichter <domseichter@web.de>
2006-08-16 14:41:03 +0000

    Remove watermark as it is now a test of its own
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@130 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- test/CreationTest/CreationTest.cpp
+++ test/CreationTest/CreationTest.cpp
@@ -316,35 +316,6 @@ void EllipseTest( PdfPainter* pPainter, PdfPage* pPage, PdfDocument* pDocument )
     pPainter->FillEllipse( dX, dY, 20000 * CONVERSION_CONSTANT, 20000 * CONVERSION_CONSTANT );
 }
 
-void WatermarkFile( const char* pszFilename )
-{
-    printf("Running watermark test\n");
-
-    PdfDocument doc( pszFilename );
-    PdfPainter  painter;
-    PdfPage*    pPage;
-    PdfRect     rect;
-    int         i;
-
-    for(i=0;i<doc.GetPageCount();i++)
-    {
-        pPage = doc.GetPage( i );
-        if( !pPage ) 
-        {
-            RAISE_ERROR( ePdfError_InvalidHandle );
-        }
-        
-        rect = pPage->PageSize();
-
-        painter.SetPage( pPage );
-        painter.SetStrokingColor( 1.0, 1.0, 1.0 );
-        painter.DrawLine( 0.0, 0.0, rect.Width(), rect.Height() );
-    }
-
-    printf("writing document back\n");
-    doc.Write( pszFilename );
-}
-
 int main( int argc, char* argv[] ) 
 {
     PdfDocument writer;
