e9e3a1584a3da376bfd2578b3a9fab6830a0c0b2
Dominik Seichter <domseichter@web.de>
2007-01-17 21:55:08 +0000

    Added PdfInputStream:
    PdfInputStream is an interface which allows to read data from a data source. Current implementations are PdfMemoryInputStream, 
    PdfFileInputStream and PdfDeviceInputStream. A PdfOutputStream will be added, too. 
    
    PdfFilers Encode and Decode methods will be ported to use only input and output streams. This will be the first step to allow 
    writing data immediately to a file. Additionally this will support to encode data step by step, so that it is not always necessary 
    to keep the complete data in memory.
    
    These interface is very simple as of now and I try to keep it as easy as possbile.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@337 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

---
+++ src/PdfInputStream.cpp
@@ -0,0 +1,105 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Dominik Seichter                                *
+ *   domseichter@web.de                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "PdfInputStream.h"
+
+#include "PdfInputDevice.h"
+
+#include <stdio.h>
+
+namespace PoDoFo {
+
+PdfFileInputStream::PdfFileInputStream( const char* pszFilename )
+{
+    m_hFile = fopen( pszFilename, "rb" );
+    if( !m_hFile ) 
+    {
+        RAISE_ERROR( ePdfError_FileNotFound );
+    }
+}
+
+PdfFileInputStream::~PdfFileInputStream()
+{
+    if( m_hFile )
+        fclose( m_hFile );
+}
+
+long PdfFileInputStream::Read( char* pBuffer, long lLen )
+{
+    if( !pBuffer ) 
+    {
+        RAISE_ERROR( ePdfError_InvalidHandle );
+    }
+
+    // return zero if EOF is reached
+    if( feof( m_hFile ) )
+        return 0;
+
+    // return the number of bytes read and read the data
+    // into pBuffer
+    return fread( pBuffer, sizeof(char), lLen, m_hFile );
+}
+
+
+PdfMemoryInputStream::PdfMemoryInputStream( const char* pBuffer, long lBufferLen )
+    : m_pBuffer( pBuffer ), m_pCur( pBuffer ), m_lBufferLen( lBufferLen )
+{
+
+}
+
+PdfMemoryInputStream::~PdfMemoryInputStream()
+{
+}
+
+long PdfMemoryInputStream::Read( char* pBuffer, long lLen )
+{
+    if( !pBuffer ) 
+    {
+        RAISE_ERROR( ePdfError_InvalidHandle );
+    }
+
+    long lRead = m_pCur - m_pBuffer;
+
+    // return zero if EOF is reached
+    if( lRead == m_lBufferLen ) 
+        return 0;
+
+    lLen = ( lRead + lLen <= m_lBufferLen ? lLen : m_lBufferLen - lRead );
+    memcpy( pBuffer, m_pCur, lLen );
+    m_pCur += lLen;
+    
+    return lLen;
+}
+
+PdfDeviceInputStream::PdfDeviceInputStream( PdfInputDevice* pDevice )
+    : m_pDevice( pDevice )
+{
+}
+
+PdfDeviceInputStream::~PdfDeviceInputStream()
+{
+}
+
+long PdfDeviceInputStream::Read( char* pBuffer, long lLen )
+{
+    return m_pDevice->Read( pBuffer, lLen );
+}
+
+};
---
+++ src/PdfInputStream.h
@@ -0,0 +1,138 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Dominik Seichter                                *
+ *   domseichter@web.de                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef _PDF_INPUT_STREAM_H_
+#define _PDF_INPUT_STREAM_H_
+
+#include "PdfDefines.h"
+
+namespace PoDoFo {
+
+class PdfInputDevice;
+
+/** An interface for reading blocks of data from an 
+ *  a data source.
+ */     
+class PODOFO_API PdfInputStream {
+ public:
+
+    virtual ~PdfInputStream();
+
+    /** Read data from the input stream
+     *  
+     *  \param pBuffer the data will be stored into this buffer
+     *  \param lLen    the size of the buffer and number of bytes
+     *                 that will be read
+     *
+     *  \returns the number of bytes read, -1 if an error ocurred
+     *           and zero if no more bytes are available for reading.
+     */
+    virtual long Read( char* pBuffer, long lLen ) = 0;
+
+};
+
+/** An input stream that reads data from a file
+ */
+class PODOFO_API PdfFileInputStream : public PdfInputStream {
+ public:
+    
+    /** Open a file for reading data
+     *  
+     *  \param pszFilename the filename of the file to read
+     */
+    PdfFileInputStream( const char* pszFilename );
+    ~PdfFileInputStream();
+
+    /** Read data from the input stream
+     *  
+     *  \param pBuffer the data will be stored into this buffer
+     *  \param lLen    the size of the buffer and number of bytes
+     *                 that will be read
+     *
+     *  \returns the number of bytes read, -1 if an error ocurred
+     *           and zero if no more bytes are available for reading.
+     */
+    virtual long Read( char* pBuffer, long lLen );
+
+ private:
+    FILE* m_hFile;
+};
+
+/** An input stream that reads data from a memory buffer
+ */
+class PODOFO_API PdfMemoryInputStream : public PdfInputStream {
+ public:
+    
+    /** Open a file for reading data
+     *  
+     *  \param pBuffer buffer to read from
+     *  \param lBufferLen length of the buffer
+     */
+    PdfMemoryInputStream( const char* pBuffer, long lBufferLen );
+    ~PdfMemoryInputStream();
+
+    /** Read data from the input stream
+     *  
+     *  \param pBuffer the data will be stored into this buffer
+     *  \param lLen    the size of the buffer and number of bytes
+     *                 that will be read
+     *
+     *  \returns the number of bytes read, -1 if an error ocurred
+     *           and zero if no more bytes are available for reading.
+     */
+    virtual long Read( char* pBuffer, long lLen );
+
+ private:
+    const char* m_pBuffer;
+    const char* m_pCur;
+    long        m_lBufferLen;
+};
+
+/** An input stream that reads data from an input device
+ */
+class PODOFO_API PdfDeviceInputStream : public PdfInputStream {
+ public:
+    
+    /** 
+     *  Read from an alread opened input device
+     * 
+     *  \param pDevice an input device
+     */
+    PdfDeviceInputStream( PdfInputDevice* pDevice );
+    ~PdfDeviceInputStream();
+
+    /** Read data from the input stream
+     *  
+     *  \param pBuffer the data will be stored into this buffer
+     *  \param lLen    the size of the buffer and number of bytes
+     *                 that will be read
+     *
+     *  \returns the number of bytes read, -1 if an error ocurred
+     *           and zero if no more bytes are available for reading.
+     */
+    virtual long Read( char* pBuffer, long lLen );
+
+ private:
+    PdfInputDevice* m_pDevice;
+};
+
+};
+
+#endif // _PDF_INPUT_STREAM_H_
