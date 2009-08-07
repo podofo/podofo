223f00bc14c7d23abb696fb60ef95043723ad883
Craig Ringer <craig@postnewspapers.com.au>
2009-08-07 07:06:08 +0000

    PdfFontMetrics included <windows.h> directly.
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1125 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- src/PdfFontMetrics.cpp
+++ src/PdfFontMetrics.cpp
@@ -18,10 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
 #include "PdfFontMetrics.h"
 
 #include "PdfArray.h"
