dd65ea30557fe825acb53a7508eedcc0d3ce9e21
Craig Ringer <craig@postnewspapers.com.au>
2009-08-24 16:34:25 +0000

    Incorrect use of pthread mutex attributes, caught by Mike Slegeir
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1140 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- src/util/PdfMutexImpl_pthread.h
+++ src/util/PdfMutexImpl_pthread.h
@@ -75,6 +75,7 @@ class PdfMutexImpl {
 
 PdfMutexImpl::PdfMutexImpl() {
     pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
     pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
     pthread_mutex_init( &m_mutex, &attr );
 }
