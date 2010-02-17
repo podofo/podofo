c688a9b07da7cb462fdb41dcef7b725a1f8141a4
Dominik Seichter <domseichter@web.de>
2010-02-17 16:42:38 +0000

    ADDED: Some more features to podofotxt2pdf. One can specify that input data is UTF8 and can therefore print unicode text now. FIXED: Start drawing on the top of the page instead of bottom. ADDED: Optional parameter to set a font name.
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1191 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- tools/podofotxt2pdf/podofotxt2pdf.cpp
+++ tools/podofotxt2pdf/podofotxt2pdf.cpp
@@ -41,27 +41,43 @@ using namespace PoDoFo;
 #define BORDER_TOP   10000 * CONVERSION_CONSTANT
 #define BORDER_LEFT  10000 * CONVERSION_CONSTANT
 #define FONT_SIZE    12.0
+#define DEFAULT_FONT "Arial"
 
 void print_help()
 {
   printf("Usage: podofotxt2pdf [inputfile] [outputfile]\n\n");
+  printf("Optional parameters:\n");
+  printf("\t-fontname [name]\t Use the font [name]\n");
+  printf("\t-utf8\t that specifies that the input text\n");
+  printf("\t\tis UTF8 encoded instead of Windows ANSI encoding.\n");       
   printf("\nPoDoFo Version: %s\n\n", PODOFO_VERSION_STRING);
 }
 
-void draw( char* pszBuffer, PdfDocument* pDocument )
+void draw( char* pszBuffer, PdfDocument* pDocument, bool bUtf8, const char* pszFontName )
 {
-    PdfPage*        pPage;
-    PdfPainter      painter;
-    PdfFont*        pFont;
-    PdfRect         size;
+    PdfPage*           pPage;
+    PdfPainter         painter;
+    PdfFont*           pFont;
+    PdfRect            size;
+    const PdfEncoding* pEncoding;
 
     double dX       = BORDER_LEFT;
     double dY       = BORDER_TOP;
     char*  pszStart = pszBuffer;
 
+    if( bUtf8 ) 
+    {
+        pEncoding = new PdfIdentityEncoding();
+    }
+    else
+    {
+        pEncoding = PdfEncodingFactory::GlobalWinAnsiEncodingInstance();
+    }
+
     size            = PdfPage::CreateStandardPageSize( ePdfPageSize_A4 );
-    pFont = pDocument->CreateFont( "Arial" );
+    pFont = pDocument->CreateFont( pszFontName, pEncoding );
     pPage = pDocument->CreatePage( size );
+    dY    = size.GetHeight() - dY;
 
     if( !pFont )
     {
@@ -76,16 +92,24 @@ void draw( char* pszBuffer, PdfDocument* pDocument )
     {
         if( *pszBuffer == '\n' )
         {
-            painter.DrawText( dX, dY, pszStart, pszBuffer-pszStart );
+            if( bUtf8 ) 
+            {
+                painter.DrawText( dX, dY, PdfString( reinterpret_cast<const pdf_utf8*>(pszStart), 
+                                                     pszBuffer-pszStart ) );
+            }
+            else
+            {
+                painter.DrawText( dX, dY, pszStart, pszBuffer-pszStart );
+            }
     
             pszStart = (++pszBuffer);            
 
-            dY += pFont->GetFontMetrics()->GetLineSpacing();
-            if( dY > (size.GetHeight() -  BORDER_TOP) )
+            dY -= pFont->GetFontMetrics()->GetLineSpacing();
+            if( dY < BORDER_TOP )
             {
                 pPage = pDocument->CreatePage( size );
                 painter.SetPage( pPage );
-                dY       = BORDER_TOP;
+                dY       = size.GetHeight() - dY;
             }
         }
         else
@@ -95,7 +119,7 @@ void draw( char* pszBuffer, PdfDocument* pDocument )
     painter.FinishPage();
 }
 
-void init( const char* pszInput, const char* pszOutput )
+void init( const char* pszInput, const char* pszOutput, bool bUtf8, const char* pszFontName )
 {
     FILE*   hFile;
 
@@ -134,7 +158,7 @@ void init( const char* pszInput, const char* pszOutput )
 
     pszBuf[lSize] = '\0';
 
-    draw( pszBuf, &doc );
+    draw( pszBuf, &doc, bUtf8, pszFontName );
 
     doc.GetInfo()->SetCreator( PdfString("podofotxt2pdf") );
     doc.GetInfo()->SetTitle( PdfString("Converted to PDF from a text file") );
@@ -145,20 +169,43 @@ void init( const char* pszInput, const char* pszOutput )
 
 int main( int argc, char* argv[] )
 {
-  char*   pszInput;
-  char*   pszOutput;
+  const char*   pszInput;
+  const char*   pszOutput;
+  const char*   pszFontName = DEFAULT_FONT;
+  bool          bUtf8 = false;
 
-  if( argc != 3 )
+  if( argc <= 3 ) 
   {
     print_help();
     exit( -1 );
   }
 
-  pszInput  = argv[1];
-  pszOutput = argv[2];
+  for(int i=1;i<argc;i++) 
+  {
+      if( strcmp("-utf8", argv[i]) == 0 ) 
+      {
+          bUtf8 = true;
+      }
+      else if( strcmp("-fontname", argv[i]) == 0 ) 
+      {
+          ++i;
+          pszFontName = argv[i];
+      }
+      else 
+      {
+          if( pszInput == NULL )
+          {
+              pszInput = argv[i];
+          }
+          else
+          {
+              pszOutput = argv[i];
+          }
+      }
+  }
 
   try {
-      init( pszInput, pszOutput );
+      init( pszInput, pszOutput, bUtf8, pszFontName );
   } catch( PdfError & e ) {
       fprintf( stderr, "Error %i occurred!\n", e.GetError() );
       e.PrintErrorMsg();
