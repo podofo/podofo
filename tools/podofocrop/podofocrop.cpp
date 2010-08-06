260270c872b4e81cd65286b29ba746950ee2063e
Dominik Seichter <domseichter@web.de>
2010-08-06 18:56:59 +0000

    Make it compile on Win32 ... not tested
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1272 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- tools/podofocrop/podofocrop.cpp
+++ tools/podofocrop/podofocrop.cpp
@@ -26,7 +26,10 @@
 #include <vector>
 
 #ifdef _WIN32
-# error "Not yet available for Windows"
+# include <windows.h>
+# ifdef GetObject
+#  undef GetObject
+# endif 
 #else
 # include <unistd.h>
 # include <sys/types.h> 
@@ -61,11 +64,54 @@ void crop_page( PdfPage* pPage, const PdfRect & rCropBox )
     pPage->GetObject()->GetDictionary().AddKey( PdfName("MediaBox"), var );
 }
 
-std::vector<PdfRect> get_crop_boxes( const char* pszInput )
+std::string get_ghostscript_output( const char* pszInput )
 {
+	std::string sOutput;
     const int lBufferLen = 256;
     char buffer[lBufferLen];
-    std::vector<PdfRect> rects;
+
+#ifdef _WIN32
+    DWORD count;
+	char cmd[lBufferLen];
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+
+    // Fenster nicht sichtbar
+    si.dwFlags=STARTF_USESHOWWINDOW;
+    si.wShowWindow=SW_HIDE;
+
+    // Ausgabe umleiten
+    HANDLE pipe;
+    CreatePipe(&pipe, 0, 0, 0 );
+    si.dwFlags|=STARTF_USESTDHANDLES;
+    //si.hStdOutput=pipe_wr;
+	si.hStdError = pipe;
+
+	
+	_snprintf(cmd, lBufferLen, "gs -dSAFER -sDEVICE=bbox -sNOPAUSE -q %s -c quit", pszInput);
+	printf("Running %s\n", cmd );
+    if( !CreateProcess( NULL, cmd, NULL, NULL, TRUE, CREATE_DEFAULT_ERROR_MODE, NULL, NULL, &si, &pi ) ) 
+    {
+		printf("CreateProcess failed.");
+        exit(1);
+    }
+
+    while( ReadFile(pipe,buffer,lBufferLen,&count,NULL) && GetLastError() != ERROR_BROKEN_PIPE && count > 0)
+    {
+		printf("%s",buffer);
+        sOutput.append( buffer, count );
+    }
+
+	// eigenes Handle schliessen
+    CloseHandle(pipe);
+	CloseHandle( pi.hProcess );
+	CloseHandle( pi.hThread ); 
+#else
     pid_t pid;
     int p[2];
     int count;
@@ -90,47 +136,54 @@ std::vector<PdfRect> get_crop_boxes( const char* pszInput )
     {
         close( p[1] ); // Close unused write end
 
-        std::string sOuput;
         while( (count = read( p[0], buffer, lBufferLen )) > 0 )
         {
-            sOuput.append( buffer, count );
+            sOutput.append( buffer, count );
         }
         wait(NULL);
+    }
 
+#endif // _WIN32
+	return sOutput;
+}
 
-        std::stringstream ss(sOuput);
-        std::string sLine;
-        PdfRect curRect;
-        bool bHaveRect = false;
-        while(std::getline(ss, sLine)) 
+std::vector<PdfRect> get_crop_boxes( const char* pszInput )
+{
+    std::vector<PdfRect> rects;
+	std::string sOutput = get_ghostscript_output( pszInput );
+
+	std::stringstream ss(sOutput);
+    std::string sLine;
+    PdfRect curRect;
+    bool bHaveRect = false;
+    while(std::getline(ss, sLine)) 
+    {
+        if( strncmp( "%%BoundingBox: ", sLine.c_str(), 15 ) == 0 )
         {
-            if( strncmp( "%%BoundingBox: ", sLine.c_str(), 15 ) == 0 )
+            int x, y, w, h;
+            sscanf(sLine.c_str()+15, "%i %i %i %i\n", &x, &y, &w, &h);
+            curRect = PdfRect( static_cast<double>(x), 
+                               static_cast<double>(y),
+                               static_cast<double>(w-x),
+                               static_cast<double>(h-y) );
+            bHaveRect = true;
+        } 
+        else if( strncmp( "%%HiResBoundingBox: ", sLine.c_str(), 17 ) == 0 ) 
+        {
+            if( bHaveRect ) 
             {
-                int x, y, w, h;
-                sscanf(sLine.c_str()+15, "%i %i %i %i\n", &x, &y, &w, &h);
-                curRect = PdfRect( static_cast<double>(x), 
-                                   static_cast<double>(y),
-                                   static_cast<double>(w-x),
-                                   static_cast<double>(h-y) );
-                bHaveRect = true;
-            } 
-            else if( strncmp( "%%HiResBoundingBox: ", sLine.c_str(), 17 ) == 0 ) 
-            {
-                if( bHaveRect ) 
-                {
-                    // I have no idea, while gs writes BoundingBoxes twice to stdout ..
-                    printf("Using bounding box: [ %f %f %f %f ]\n", 
-                           curRect.GetLeft(),
-                           curRect.GetBottom(),
-                           curRect.GetWidth(),
-                           curRect.GetHeight());
-                    rects.push_back( curRect );
-                    bHaveRect = false;
-                }
+                // I have no idea, while gs writes BoundingBoxes twice to stdout ..
+                printf("Using bounding box: [ %f %f %f %f ]\n", 
+                       curRect.GetLeft(),
+                       curRect.GetBottom(),
+                       curRect.GetWidth(),
+                       curRect.GetHeight());
+                rects.push_back( curRect );
+                bHaveRect = false;
             }
         }
     }
-
+	
     return rects;
 }
 
