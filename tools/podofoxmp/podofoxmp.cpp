13dd2222088ba49da35c49c2a0ba7157d0cfa856
Dominik Seichter <domseichter@web.de>
2010-08-10 11:02:53 +0000

    ADDED: PoDoFo XMP tool for setting and extracting XMP information. Thanks to Ian Ashley for the contribution.
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1273 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

---
+++ tools/podofoxmp/podofoxmp.cpp
@@ -0,0 +1,106 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Ian Ashley                                      *
+ *   Ian Ashley <Ian.Ashley@opentext.com>                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+#include "podofo.h"
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	if (argc != 2 && argc != 4)
+    {
+		cout << "Syntax" << endl;
+		cout << "  " << argv[0] << " <pdf file> - display the XMP in a file" << endl;
+		cout << "or" << endl;
+		cout << "  " << argv[0] << " <src pdf file> <xmp file> <new pdf file> - create a new PDF with the XMP in" << endl;
+		return EXIT_FAILURE;
+    }
+
+    PoDoFo::PdfError::EnableDebug( false );
+    try {
+        PoDoFo::PdfMemDocument doc(argv[1]);
+
+        if (argc == 2)
+        {
+            PoDoFo::PdfObject *metadata;
+            if ((metadata = doc.GetMetadata()) == NULL)
+            {
+                cout << "No metadata" << endl;
+            }
+            else
+            {
+                PoDoFo::PdfStream *str = metadata->GetStream();
+                if (str != NULL)
+				{
+                    char *buf;
+                    PoDoFo::pdf_long len;
+                    
+                    str->GetFilteredCopy(&buf, &len);
+                    for (PoDoFo::pdf_long i = 0; i < len; ++i)
+                    {
+                        printf("%c", buf[i]);
+                    }
+                    printf("\n");
+                    fflush(stdout);
+                    free(buf);
+				}
+            }
+        }
+        
+        if (argc == 4)
+        {
+            char *xmpBuf;
+            FILE *fp;
+            
+            if ((fp = fopen(argv[2], "rb")) == NULL)
+            {
+                cout << "Cannot open " << argv[2] << endl;
+            }
+            else
+            {
+                fseek(fp, 0, SEEK_END);
+                long xmpLen = ftell(fp);
+                xmpBuf = new char[xmpLen];
+                fseek(fp, 0, SEEK_SET);
+                fread(xmpBuf, 1, xmpLen, fp);
+                fclose(fp);
+                
+                PoDoFo::PdfObject *xmpObj = doc.GetObjects().CreateObject("Metadata");
+                xmpObj->GetDictionary().AddKey(PoDoFo::PdfName("Subtype"), PoDoFo::PdfName("XML"));
+                xmpObj->GetStream()->Set(xmpBuf, xmpLen);
+                doc.GetCatalog()->GetDictionary().AddKey(PoDoFo::PdfName("Metadata"), xmpObj->Reference());
+                delete[] xmpBuf;
+                
+                doc.Write(argv[3]);
+			}
+        }
+	}
+	catch( PoDoFo::PdfError & e )
+	{
+		cerr << "Error: An error "<< e.GetError() <<" ocurred during processing the pdf file\n";
+		e.PrintErrorMsg();
+		return e.GetError();
+	}
+	return EXIT_SUCCESS;
+}
