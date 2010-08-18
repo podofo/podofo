4c063947a639cc98d57e1e678569073b12d8cf8a
Dominik Seichter <domseichter@web.de>
2010-08-18 10:18:05 +0000

    FIXED: Do not corrupt heap FIXED: Use function _vscprintf to determine length of format string without allocating a buffer on newer windows compilers
    Thanks to Olaf for these patches
    
    
    
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1282 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- src/PdfOutputDevice.cpp
+++ src/PdfOutputDevice.cpp
@@ -153,11 +153,16 @@ void PdfOutputDevice::Print( const char* pszFormat, ... )
     else
     {
         va_start( args, pszFormat );
-#if (defined _MSC_VER || defined __hpux)	// vsnprintf without buffer does not work with MS-VC or HPUX
+        // OC 17.08.2010: Use new function _vscprintf to get the number of characters:
+        // visual c++  8.0 == 1400 (Visual Studio 2005)
+        // i am not shure if 1300 is ok here, but who cares this cruel compiler version
+#if (defined _MSC_VER && _MSC_VER >= 1400 )
+        lBytes = _vscprintf( pszFormat, args );
+#elif (defined _MSC_VER || defined __hpux)  // vsnprintf without buffer does not work with MS-VC or HPUX
         int len = 1024;
         do
         {
-            char * temp = new char[len];
+            char * temp = new char[len+1]; // OC 17.08.2010 BugFix: +1 avoids corrupted heap
             lBytes = vsnprintf( temp, len+1, pszFormat, args );
             delete[] temp;
             len *= 2;
