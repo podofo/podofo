854e050e75aef08655b1ae104674684dfa4e023b
Dominik Seichter <domseichter@web.de>
2010-08-18 10:27:22 +0000

    Patch by Olaf, initialize Debug Heap to use memory leak detection in visual studio
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1285 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- src/PdfMemoryManagement.cpp
+++ src/PdfMemoryManagement.cpp
@@ -19,6 +19,7 @@
  ***************************************************************************/
 
 #include "PdfMemoryManagement.h"
+#include "PdfDefinesPrivate.h"
 
 namespace PoDoFo {
 
@@ -38,3 +39,44 @@ void podofo_free( void* buffer )
 }
 
 };
+
+// OC 17.08.2010: Activate showing the correct source for Memory Leak Detection in Visual Studio:
+// See: <afx.h>  looking for _AFX_NO_DEBUG_CRT
+//
+// Annotation:
+// defining the new and delete operators and initializeDebugHeap
+// have to be done inside the dll if podofo is compiled as dll.
+//
+// If podofo is compiled as static library
+// the main program is responsible for theese definitions and initialisations.
+// Specially the new operator may be implemented completely different
+// and may not be redefined here.
+#ifdef _MSC_VER
+#ifdef DEBUG_NEW // #if defined(_DEBUG) && defined(DEFINE_NEW_DEBUG_NEW)
+#if defined(COMPILING_SHARED_PODOFO) || defined(podofo_shared_EXPORTS)
+
+#undef new
+void* operator new(size_t ai_NewSize, const char* ac_File_, int ai_Line)
+{
+    return ::operator new(ai_NewSize, _NORMAL_BLOCK, ac_File_, ai_Line);
+}
+void operator delete(void* av_Ptr_, const char* ac_File_, int ai_Line)
+{
+    ::operator delete(av_Ptr_);
+}
+#define new DEBUG_NEW
+
+static int initializeDebugHeap()
+{
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // _CRTDBG_CHECK_ALWAYS_DF|_CRTDBG_DELAY_FREE_MEM_DF
+  // test if the memory leak detection works:
+  static const char leakdata[] = "*** test memory leak ***";
+  char* memleak = new char[sizeof(leakdata)];
+  memcpy(memleak, leakdata, sizeof(leakdata));
+  return 1;
+}
+int isDebugHeapInitialized = initializeDebugHeap();
+
+#endif // podofo_shared_EXPORTS
+#endif // DEBUG_NEW
+#endif // _MSC_VER
