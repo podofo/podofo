353164b95482cf14357ff38a4b9c8e58f1d43ba4
Dominik Seichter <domseichter@web.de>
2010-10-20 17:02:38 +0000

    FIXED: Predictor implementations and support some more predictors
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1337 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- src/PdfFiltersPrivate.cpp
+++ src/PdfFiltersPrivate.cpp
@@ -83,7 +83,8 @@ public:
 
         m_nCurPredictor = -1;
         m_nCurRowIndex  = 0;
-        m_nRows = (m_nColumns * m_nBPC) >> 3; 
+        m_nBpp  = (m_nBPC * m_nColors) >> 3;
+        m_nRows = (m_nColumns * m_nColors * m_nBPC) >> 3;
 
         m_pPrev = static_cast<char*>(malloc( sizeof(char) * m_nRows));
         if( !m_pPrev )
@@ -121,6 +122,7 @@ public:
             {
                 m_nCurRowIndex  = 0;
                 m_nCurPredictor = m_nPredictor >= 10 ? *pBuffer + 10 : *pBuffer;
+                printf("3 m_nPredictor=%i m_nCurPredictor=%i\n", m_nPredictor, m_nCurPredictor );
                 pStream->Write( m_pPrev, m_nRows );
             }
             else
@@ -129,20 +131,40 @@ public:
                 {
                     case 2: // Tiff Predictor
                         // TODO: implement tiff predictor
+                        PODOFO_RAISE_ERROR( ePdfError_InvalidPredictor );
                         break;
                     case 10: // png none
+                    {
+                        m_pPrev[m_nCurRowIndex] = *pBuffer;
+                        break;
+                    }
                     case 11: // png sub
+                    {
+                        int prev = (m_nCurRowIndex - m_nBpp < 0 
+                                    ? 0 : m_pPrev[m_nCurRowIndex - m_nBpp]);
+                        m_pPrev[m_nCurRowIndex] = *pBuffer + prev;
+                        break;
+                    }
                     case 12: // png up
+                    {
                         m_pPrev[m_nCurRowIndex] += *pBuffer;
                         break;
+                    }
                     case 13: // png average
+                    {
+                        int prev = (m_nCurRowIndex - m_nBpp < 0 
+                                    ? 0 : m_pPrev[m_nCurRowIndex - m_nBpp]);
+                        m_pPrev[m_nCurRowIndex] = ((prev + m_pPrev[m_nCurRowIndex]) >> 1) + *pBuffer;
+                        break;
+                    }
                     case 14: // png paeth
                     case 15: // png optimum
+                        PODOFO_RAISE_ERROR( ePdfError_InvalidPredictor );
                         break;
                         
                     default:
                     {
-                        PODOFO_RAISE_ERROR( ePdfError_InvalidPredictor );
+                        //PODOFO_RAISE_ERROR( ePdfError_InvalidPredictor );
                         break;
                     }
                 }
@@ -159,9 +181,10 @@ public:
 private:
     int m_nPredictor;
     int m_nColors;
-    int m_nBPC;
+    int m_nBPC; //< Bytes per component
     int m_nColumns;
     int m_nEarlyChange;
+    int m_nBpp; ///< Bytes per pixel
 
     int m_nCurPredictor;
     int m_nCurRowIndex;
