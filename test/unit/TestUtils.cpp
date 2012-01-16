f599cb0e1876c90e635ba041a291721853ecc77b
Dominik Seichter <domseichter@web.de>
2012-01-16 20:41:26 +0000

    FIXED: Build with gcc 4.7, thanks to Dan Horak
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1493 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- test/unit/TestUtils.cpp
+++ test/unit/TestUtils.cpp
@@ -17,12 +17,12 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #include "TestUtils.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
