01abeb70915f304cbeca1aefb08536476609dda2
zyx <zyx@gmx.us>
2016-05-12 20:08:20 +0000

    Add spaces between literal and macro for C++11 compatibility
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1726 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- test/unit/PagesTreeTest.cpp
+++ test/unit/PagesTreeTest.cpp
@@ -361,7 +361,7 @@ bool PagesTreeTest::IsPageNumber( PoDoFo::PdfPage* pPage, int nNumber )
 
     if( lPageNumber != static_cast<pdf_int64>(nNumber) )
     {
-        printf("PagesTreeTest: Expected page number %i but got %"PDF_FORMAT_INT64".\n", nNumber, lPageNumber);
+        printf("PagesTreeTest: Expected page number %i but got %" PDF_FORMAT_INT64 ".\n", nNumber, lPageNumber);
         return false;
     }
     else
