cd1e62e7f113ea62deeb726b7a2d3d8905d282bb
zyx <zyx@gmx.us>
2018-01-28 15:55:04 +0000

    Related to CVE-2018-5308: Add in-parameter validity check
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/trunk@1876 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- src/base/PdfOutputStream.cpp
+++ src/base/PdfOutputStream.cpp
@@ -85,6 +85,11 @@ PdfMemoryOutputStream::PdfMemoryOutputStream( pdf_long lInitial )
 PdfMemoryOutputStream::PdfMemoryOutputStream( char* pBuffer, pdf_long lLen )
     : m_lLen( 0 ), m_bOwnBuffer( false )
 {
+    if( !pBuffer ) 
+    {
+        PODOFO_RAISE_ERROR( ePdfError_InvalidHandle );
+    }
+
     m_lSize   = lLen;
     m_pBuffer = pBuffer;
 }
