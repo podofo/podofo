45cdfb4b8ab2b6a51374a511b15d2a7e9500c8f3
Francesco Pretto <ceztko@gmail.com>
2019-04-04 21:22:16 +0000

    Moved source from root/src to root/src/podofo
    This allow to remove need for wrapper headers in root/podofo,
    allowing to include headers directly from the source directory.
    
    The patch does:
    - Move all source from src/* to src/podofo/*;
    - Fix compilation in CMakeLists.txt;
    - Fix compilation of FormTest.cpp and SignTest.cpp;
    - Delete the root/podofo header wrappers folder.
    
    It also:
    - Removes unreferenced compilercompat header folder;
    - Fixes line ending of PdfSignOutputDevice.cpp and PdfTilingPattern.h to LF while moving files;
    
    git-svn-id: https://svn.code.sf.net/p/podofo/code/podofo/branches/SrcPodofoLayout@1977 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- test/SignatureTest/SignTest.cpp
+++ test/SignatureTest/SignTest.cpp
@@ -19,7 +19,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "../../src/podofo.h"
+#include <podofo.h>
 
 #include "../PdfTest.h"
 
