podofo/podofo

commit 47f010b4225f25fe7b033fa888eea71090c944f0

PdfEncrypt review
- Renamed PdfEncryptAlgorithm::AESV3 -> PdfEncryptAlgorithm::AESV3R5
- Removed SetEnabledEncryptionAlgorithms(): Disabling algorithms is not supported anymore
- Removed CreateFromEncrypt(): Internal usage only
- Removed GetUserPassword(), GetOwnerPassword(): sensitive content, one shouldn't be able to retrieve again after setting;
- Removed PdfAESV3Revision: Internal usage only
- Removed PdfEncryptSHABase: Not needed
- Removed PdfEncryptAESBase, PdfEncryptRC4Base: Implementation details, moved to composition instead of multiple inheritance
- PdfEncryptRC4, PdfEncryptAESV2, PdfEncryptAESV3 are now final
- Removed PdfEncryptRC4, PdfEncryptAESV2, PdfEncryptAESV3 public constructors: Implementation details,
  instances are not supposed to be created by API users
- PdfEncryptMD5Base: Removed GetMD5Binary, GetMD5String: They are not supposed to be part of a PDF library

test/unit/EncryptTest.cpp
@@ -46,14 +46,14 @@ struct Paths
     }
 } s_init;
 
-TEST_CASE("testDefault")
+TEST_CASE("TestDefaultEncryption")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD);
     testAuthenticate(*encrypt);
     testEncrypt(*encrypt);
 }
 
-TEST_CASE("testRC4")
+TEST_CASE("TestRC4")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
         PdfEncryptAlgorithm::RC4V1,
@@ -63,7 +63,7 @@ TEST_CASE("testRC4")
     testEncrypt(*encrypt);
 }
 
-TEST_CASE("testRC4v2_40")
+TEST_CASE("TestRC4v2_40")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
         PdfEncryptAlgorithm::RC4V2,
@@ -73,7 +73,7 @@ TEST_CASE("testRC4v2_40")
     testEncrypt(*encrypt);
 }
 
-TEST_CASE("testRC4v2_56")
+TEST_CASE("TestRC4v2_56")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
         PdfEncryptAlgorithm::RC4V2,
@@ -83,7 +83,7 @@ TEST_CASE("testRC4v2_56")
     testEncrypt(*encrypt);
 }
 
-TEST_CASE("testRC4v2_80")
+TEST_CASE("TestRC4v2_80")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
         PdfEncryptAlgorithm::RC4V2,
@@ -93,7 +93,7 @@ TEST_CASE("testRC4v2_80")
     testEncrypt(*encrypt);
 }
 
-TEST_CASE("testRC4v2_96")
+TEST_CASE("TestRC4v2_96")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
         PdfEncryptAlgorithm::RC4V2,
@@ -103,7 +103,7 @@ TEST_CASE("testRC4v2_96")
     testEncrypt(*encrypt);
 }
 
-TEST_CASE("testRC4v2_128")
+TEST_CASE("TestRC4v2_128")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
         PdfEncryptAlgorithm::RC4V2,
@@ -113,7 +113,7 @@ TEST_CASE("testRC4v2_128")
     testEncrypt(*encrypt);
 }
 
-TEST_CASE("testAESV2")
+TEST_CASE("TestAESV2")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
         PdfEncryptAlgorithm::AESV2,
@@ -127,10 +127,10 @@ TEST_CASE("testAESV2")
 
 #ifdef PODOFO_HAVE_LIBIDN
 
-TEST_CASE("testAESV3")
+TEST_CASE("TestAESV3R5")
 {
     auto encrypt = PdfEncrypt::Create(PDF_USER_PASSWORD, PDF_OWNER_PASSWORD, s_protection,
-        PdfEncryptAlgorithm::AESV3,
+        PdfEncryptAlgorithm::AESV3R5,
         PdfKeyLength::L256);
 
     testAuthenticate(*encrypt);
@@ -155,55 +155,21 @@ TEST_CASE("testAESV3R6")
 
 TEST_CASE("testEnableAlgorithms")
 {
-    auto enabledAlgorithms = PdfEncrypt::GetEnabledEncryptionAlgorithms();
-
     // By default every algorithms should be enabled
     REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::RC4V1));
     REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::RC4V2));
     REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::AESV2));
 #ifdef PODOFO_HAVE_LIBIDN
-    REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::AESV3));
+    REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::AESV3R5));
     REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::AESV3R6));
 #endif // PODOFO_HAVE_LIBIDN
 
     PdfEncryptAlgorithm testAlgorithms = PdfEncryptAlgorithm::AESV2;
     testAlgorithms |= PdfEncryptAlgorithm::RC4V1 | PdfEncryptAlgorithm::RC4V2;
 #ifdef PODOFO_HAVE_LIBIDN
-    testAlgorithms |= PdfEncryptAlgorithm::AESV3 | PdfEncryptAlgorithm::AESV3R6;;
+    testAlgorithms |= PdfEncryptAlgorithm::AESV3R5 | PdfEncryptAlgorithm::AESV3R6;;
 #endif // PODOFO_HAVE_LIBIDN
     REQUIRE(testAlgorithms == PdfEncrypt::GetEnabledEncryptionAlgorithms());
-
-    // Disable AES
-    PdfEncrypt::SetEnabledEncryptionAlgorithms(PdfEncryptAlgorithm::RC4V1 |
-        PdfEncryptAlgorithm::RC4V2);
-
-    REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::RC4V1));
-    REQUIRE(PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::RC4V2));
-    REQUIRE(!PdfEncrypt::IsEncryptionEnabled(PdfEncryptAlgorithm::AESV2));
-
-    REQUIRE((PdfEncryptAlgorithm::RC4V1 | PdfEncryptAlgorithm::RC4V2) ==
-        PdfEncrypt::GetEnabledEncryptionAlgorithms());
-
-    PdfObject object;
-    object.GetDictionary().AddKey("Filter", PdfName("Standard"));
-    object.GetDictionary().AddKey("V", static_cast<int64_t>(4L));
-    object.GetDictionary().AddKey("R", static_cast<int64_t>(4L));
-    object.GetDictionary().AddKey("P", static_cast<int64_t>(1L));
-    object.GetDictionary().AddKey("O", PdfString(""));
-    object.GetDictionary().AddKey("U", PdfString(""));
-
-    try
-    {
-        (void)PdfEncrypt::CreateFromObject(object);
-        REQUIRE(false);
-    }
-    catch (PdfError& error)
-    {
-        REQUIRE(error.GetCode() == PdfErrorCode::UnsupportedFilter);
-    }
-
-    // Restore default
-    PdfEncrypt::SetEnabledEncryptionAlgorithms(enabledAlgorithms);
 }
 
 TEST_CASE("testLoadEncrypedFilePdfParser")
@@ -297,11 +263,11 @@ void testAuthenticate(PdfEncrypt& encrypt)
     encrypt.GenerateEncryptionKey(documentId);
 
     INFO("authenticate using user password");
-    REQUIRE(encrypt.Authenticate(PDF_USER_PASSWORD, documentId));
+    REQUIRE(encrypt.Authenticate(PDF_USER_PASSWORD, documentId) == PdfAuthResult::User);
     INFO("authenticate using owner password");
-    REQUIRE(encrypt.Authenticate(PDF_OWNER_PASSWORD, documentId));
+    REQUIRE(encrypt.Authenticate(PDF_OWNER_PASSWORD, documentId) == PdfAuthResult::Owner);
     INFO("authenticate using wrong password");
-    REQUIRE(!encrypt.Authenticate("wrongpassword", documentId));
+    REQUIRE(encrypt.Authenticate("wrongpassword", documentId) == PdfAuthResult::Failed);
 }
 
 void testEncrypt(PdfEncrypt& encrypt)

src/podofo/main/PdfParser.cpp
@@ -652,8 +652,8 @@ void PdfParser::ReadObjects(InputStreamDevice& device)
         }
 
         // Generate encryption keys
-        bool isAuthenticated = m_Encrypt->Authenticate(m_Password, this->getDocumentId());
-        if (!isAuthenticated)
+        auto result = m_Encrypt->Authenticate(m_Password, this->getDocumentId());
+        if (result == PdfAuthResult::Failed)
         {
             // authentication failed so we need a password from the user.
             // The user can set the password using PdfParser::SetPassword

src/podofo/private/OpenSSLInternal.cpp
@@ -320,14 +320,6 @@ void ssl::GetOpenSSLError(string& err)
 const EVP_CIPHER* ssl::Rc4()
 {
     ssl::Init();
-#if OPENSSL_VERSION_MAJOR >= 3
-    if (s_SSL.GetRc4() == nullptr)
-    {
-        PODOFO_RAISE_ERROR_INFO(PdfErrorCode::OpenSSL, "OpenSSL RC4 legacy provider was not found. "
-            "Recompile OpenSSL or ensure OPENSSL_MODULES variable is correctly set to load "
-            "legacy providers (e.g. legacy.dll)");
-    }
-#endif // OPENSSL_VERSION_MAJOR >= 3
     return s_SSL.GetRc4();
 }
 

tools/podofoencrypt/podofoencrypt.cpp
@@ -29,7 +29,7 @@ void encrypt(const string_view& inputPath, const string_view& outputPath,
             break;
 #endif // PODOFO_HAVE_OPENSSL_NO_RC4
 #ifdef PODOFO_HAVE_LIBIDN
-        case PdfEncryptAlgorithm::AESV3:;
+        case PdfEncryptAlgorithm::AESV3R5:;
             keyLength = PdfKeyLength::L256;
             version = PdfVersion::V1_3;
             break;
@@ -106,7 +106,7 @@ void Main(const cspan<string_view>& args)
                     algorithm = PdfEncryptAlgorithm::AESV2;
 #ifdef PODOFO_HAVE_LIBIDN
                 else if (args[i] == "--aesv3")
-                    algorithm = PdfEncryptAlgorithm::AESV3;
+                    algorithm = PdfEncryptAlgorithm::AESV3R5;
 #endif // PODOFO_HAVE_LIBIDN
                 else if (args[i] == "-u")
                 {

commit 67b78179bbe61851174cf35271ccc1080b2b5637

PdfEncrypt: Fixed serialization of /UE,/OE values for AESV3, fixed serialization of /P
They regressed in 47f010b4225f25fe7b033fa888eea71090c944f0. Added more realistic
encrypt/decrypt tests involving creating/opening real PDFs

src/podofo/main/PdfEncrypt.cpp
@@ -490,8 +490,8 @@ unique_ptr<PdfEncrypt> PdfEncrypt::CreateFromObject(const PdfObject& encryptObj)
         lV = static_cast<unsigned>(encryptObj.GetDictionary().MustGetKey("V").GetNumber());
         rValue = static_cast<unsigned>(encryptObj.GetDictionary().MustGetKey("R").GetNumber());
 
-        // The value of the P entry shall be interpreted as an unsigned
-        // 32-bit quantity containing a set of flags
+        // "The value of the P entry shall be interpreted as an unsigned
+        // 32-bit quantity containing a set of flags"
         pValue = static_cast<PdfPermissions>(encryptObj.GetDictionary().MustGetKey("P").GetNumber() & 0xFFFFFFFF);
 
         oValue = encryptObj.GetDictionary().MustGetKey("O").GetString();
@@ -598,6 +598,17 @@ PdfEncrypt::PdfEncrypt() :
     m_oValueSize(0),
     m_EncryptMetadata(false)
 {
+}
+
+int64_t PdfEncrypt::GetPValueForSerialization() const
+{
+    // NOTE: While "The value of the P entry shall be
+    // interpreted as an unsigned 32-bit quantity", PDFs
+    // tend to write it a signed integer, which is weird
+    // but still acceptable. We convert it first to int32_t
+    // again before casting to a PDF 64 bit number to
+    // preserve the same form
+    return (int64_t)(int32_t)m_pValue;
 }
 
 void PdfEncrypt::Init(PdfEncryptionAlgorithm algorithm, PdfKeyLength keyLength, unsigned char revision, PdfPermissions pValue,
@@ -984,7 +995,7 @@ void PdfEncryptMD5Base::CreateEncryptionDictionary(PdfDictionary& dictionary) co
 
     dictionary.AddKey("O", PdfString::FromRaw({ reinterpret_cast<const char*>(this->GetOValueRaw()), 32 }));
     dictionary.AddKey("U", PdfString::FromRaw({ reinterpret_cast<const char*>(this->GetUValueRaw()), 32 }));
-    dictionary.AddKey("P", PdfVariant(static_cast<int64_t>(this->GetPValue())));
+    dictionary.AddKey("P", PdfVariant(GetPValueForSerialization()));
 }
     
 void PdfEncryptRC4::GenerateEncryptionKey(const string_view& documentId,
@@ -1635,10 +1646,10 @@ void PdfEncryptAESV3::CreateEncryptionDictionary(PdfDictionary& dictionary) cons
     dictionary.AddKey("StrF", PdfName("StdCF"));
     dictionary.AddKey("StmF", PdfName("StdCF"));
 
-    dictionary.AddKey("P", PdfVariant(static_cast<int64_t>(this->GetPValue())));
+    dictionary.AddKey("P", PdfVariant(GetPValueForSerialization()));
 
-    dictionary.AddKey("OE", PdfString::FromRaw(GetUEValue()));
-    dictionary.AddKey("UE", PdfString::FromRaw(GetOEValue()));
+    dictionary.AddKey("OE", PdfString::FromRaw(GetOEValue()));
+    dictionary.AddKey("UE", PdfString::FromRaw(GetUEValue()));
     dictionary.AddKey("Perms", PdfString::FromRaw(GetPermsValue()));
 }
 

src/podofo/main/PdfEncrypt.h
@@ -370,6 +370,8 @@ protected:
      */
     inline const unsigned char* GetEncryptionKey() const { return m_encryptionKey; }
 
+    int64_t GetPValueForSerialization() const;
+
 protected:
     void Init(PdfEncryptionAlgorithm algorithm, PdfKeyLength keyLength, unsigned char revision,
         PdfPermissions pValue, const bufferview& uValue, const bufferview& oValue,

test/unit/EncryptTest.cpp
@@ -20,6 +20,8 @@ static void createEncryptedPdf(const string_view& filename);
 charbuff s_encBuffer;
 PdfPermissions s_protection;
 
+constexpr string_view ReferenceHash("298ACCFDC32BB2BC32BFD580883219AB");
+
 #define PDF_USER_PASSWORD "user"
 #define PDF_OWNER_PASSWORD "podofo"
 
@@ -50,8 +52,6 @@ struct Paths
 
 TEST_CASE("TestEncryptedPDFs")
 {
-    constexpr string_view ReferenceHash("298ACCFDC32BB2BC32BFD580883219AB");
-
     charbuff buffer;
     PdfMemDocument doc;
     doc.Load(TestUtils::GetTestInputFilePath("TemplateClearText.pdf"));
@@ -84,6 +84,52 @@ TEST_CASE("TestEncryptedPDFs")
         doc.Load(path, "ownerpass");
         doc.GetObjects().MustGetObject(PdfReference(11, 0)).MustGetStream().CopyTo(buffer);
         REQUIRE(ssl::ComputeMD5Str(buffer) == ReferenceHash);
+    }
+}
+
+TEST_CASE("TestEncryptDecryptPDFs")
+{
+    PdfEncryptionAlgorithm algorithms[] = {
+        PdfEncryptionAlgorithm::RC4V2,
+        PdfEncryptionAlgorithm::RC4V2,
+        PdfEncryptionAlgorithm::RC4V2,
+        PdfEncryptionAlgorithm::RC4V2,
+        PdfEncryptionAlgorithm::RC4V2,
+        PdfEncryptionAlgorithm::AESV2,
+        PdfEncryptionAlgorithm::AESV3R6,
+    };
+
+    PdfKeyLength keyLengths[] = {
+        PdfKeyLength::L40,
+        PdfKeyLength::L56,
+        PdfKeyLength::L80,
+        PdfKeyLength::L96,
+        PdfKeyLength::L128,
+        PdfKeyLength::L128,
+        PdfKeyLength::L256,
+    };
+
+    charbuff pdfBuffer;
+    charbuff objBuffer;
+    for (unsigned i = 0; i < std::size(algorithms); i++)
+    {
+        PdfMemDocument doc;
+
+        doc.Load(TestUtils::GetTestInputFilePath("TemplateClearText.pdf"));
+        doc.SetEncrypted("userpass", "ownerpass", PdfPermissions::Default,
+            algorithms[i], keyLengths[i]);
+
+        pdfBuffer.clear();
+        BufferStreamDevice device(pdfBuffer);
+        doc.Save(device);
+
+        doc.LoadFromBuffer(pdfBuffer, "userpass");
+        doc.GetObjects().MustGetObject(PdfReference(11, 0)).MustGetStream().CopyTo(objBuffer);
+        REQUIRE(ssl::ComputeMD5Str(objBuffer) == ReferenceHash);
+
+        doc.LoadFromBuffer(pdfBuffer, "ownerpass");
+        doc.GetObjects().MustGetObject(PdfReference(11, 0)).MustGetStream().CopyTo(objBuffer);
+        REQUIRE(ssl::ComputeMD5Str(objBuffer) == ReferenceHash);
     }
 }
 