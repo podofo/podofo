ebe1e51008b8768991b651a37848aac06ec61e1f
Kira M. Backes <kira.backes@nrwsoft.de>
2024-10-17 11:49:25 +0200

    Fix text underline (and probably strike-through) incompatibility with poppler

--- src/podofo/main/PdfPainter.cpp
+++ src/podofo/main/PdfPainter.cpp
@@ -332,8 +332,6 @@ void PdfPainter::DrawText(const string_view& str, double x, double y,
 
 void PdfPainter::drawText(const string_view& str, double x, double y, bool isUnderline, bool isStrikeThrough)
 {
-    PoDoFo::WriteOperator_Td(m_stream, x, y);
-
     auto& textState = m_StateStack.Current->TextState;
     auto& font = *textState.Font;
     auto expStr = this->expandTabs(str);
@@ -364,6 +362,8 @@ void PdfPainter::drawText(const string_view& str, double x, double y, bool isUnd
 
         this->restore();
     }
+
+    PoDoFo::WriteOperator_Td(m_stream, x, y);
 
     PoDoFo::WriteOperator_Tj(m_stream, font.GetEncoding().ConvertToEncoded(str),
         !font.GetEncoding().IsSimpleEncoding());
