bf58003eee5b3134bb95b399389feb993c30c4f2
Francesco Pretto <ceztko@gmail.com>
2024-10-22 23:12:05 +0200

    PdfFontCIDTrueType: Don't create a bogus /CIDSet for PDF/A level greater or equal to PDF/A-2
    Starting from  PDF/A-2 is no longer required, and if it's present it has silly requirements. Fixes #202

--- src/podofo/main/PdfFontCIDTrueType.cpp
+++ src/podofo/main/PdfFontCIDTrueType.cpp
@@ -41,29 +41,35 @@ void PdfFontCIDTrueType::embedFontSubset()
     FontTrueTypeSubset::BuildFont(buffer, GetMetrics(), gids);
     EmbedFontFileTrueType(GetDescriptor(), buffer);
 
-    // We prepare the /CIDSet content now. NOTE: The CIDSet
-    // entry is optional and it's actually deprecated in PDF 2.0
-    // but it's required for PDFA/1 compliance in TrueType CID fonts
-    string cidSetData;
-    for (auto& pair : usedGIDs)
+    auto pdfaLevel = GetDocument().GetMetadata().GetPdfALevel();
+    if (pdfaLevel == PdfALevel::L1A || pdfaLevel == PdfALevel::L1B)
     {
-        // ISO 32000-1:2008: Table 124 – Additional font descriptor entries for CIDFonts
-        // CIDSet "The stream’s data shall be organized as a table of bits
-        // indexed by CID. The bits shall be stored in bytes with the
-        // high - order bit first.Each bit shall correspond to a CID.
-        // The most significant bit of the first byte shall correspond
-        // to CID 0, the next bit to CID 1, and so on"
+        // We prepare the /CIDSet content now. NOTE: The CIDSet
+        // entry is optional and it's actually deprecated in PDF 2.0
+        // but it's required for PDF/A-1 compliance in TrueType CID fonts.
+        // Newer compliances remove this requirement, but if present
+        // it has even sillier requirements
+        string cidSetData;
+        for (auto& pair : usedGIDs)
+        {
+            // ISO 32000-1:2008: Table 124 – Additional font descriptor entries for CIDFonts
+            // CIDSet "The stream’s data shall be organized as a table of bits
+            // indexed by CID. The bits shall be stored in bytes with the
+            // high - order bit first.Each bit shall correspond to a CID.
+            // The most significant bit of the first byte shall correspond
+            // to CID 0, the next bit to CID 1, and so on"
 
-        static const char bits[] = { '\x80', '\x40', '\x20', '\x10', '\x08', '\x04', '\x02', '\x01' };
-        unsigned gid = pair.second.Id;
-        unsigned dataIndex = gid >> 3;
-        if (cidSetData.size() < dataIndex + 1)
-            cidSetData.resize(dataIndex + 1);
+            static const char bits[] = { '\x80', '\x40', '\x20', '\x10', '\x08', '\x04', '\x02', '\x01' };
+            unsigned cid = pair.second.Id;
+            unsigned dataIndex = cid >> 3;
+            if (cidSetData.size() < dataIndex + 1)
+                cidSetData.resize(dataIndex + 1);
 
-        cidSetData[dataIndex] |= bits[gid & 7];
+            cidSetData[dataIndex] |= bits[cid & 7];
+        }
+
+        auto& cidSetObj = this->GetObject().GetDocument()->GetObjects().CreateDictionaryObject();
+        cidSetObj.GetOrCreateStream().SetData(cidSetData);
+        GetDescriptor().GetDictionary().AddKeyIndirect("CIDSet"_n, cidSetObj);
     }
-
-    auto& cidSetObj = this->GetObject().GetDocument()->GetObjects().CreateDictionaryObject();
-    cidSetObj.GetOrCreateStream().SetData(cidSetData);
-    GetDescriptor().GetDictionary().AddKeyIndirect("CIDSet"_n, cidSetObj);
 }
