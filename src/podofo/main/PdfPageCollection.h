455abb1c047db80e0f5731537780b6cf22e7b2ef
Francesco Pretto <ceztko@gmail.com>
2024-11-30 11:28:12 +0100

    PdfPageCollection: Disable copy/assignment
    Fixes https://github.com/podofo/podofo/issues/218

--- src/podofo/main/PdfPageCollection.h
+++ src/podofo/main/PdfPageCollection.h
@@ -157,6 +157,9 @@ private:
     unsigned traversePageTreeNode(PdfObject& obj, unsigned count,
         std::vector<PdfObject*>& parents, std::unordered_set<PdfObject*>& visitedNodes);
 
+    PdfPageCollection(PdfPageCollection&) = delete;
+    PdfPageCollection& operator=(PdfPageCollection&) = delete;
+
 private:
     bool m_initialized;
     std::vector<PdfPage*> m_Pages;
