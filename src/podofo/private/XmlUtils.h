af925c90b7cdb14ce1230461d6f28dd4b4f913b4
Nicholas Orr <nick@goya.com.au>
2025-11-15 21:03:35 +1100

    Added the parser.h include from libxml in XmlUtils.h

--- src/podofo/private/XmlUtils.h
+++ src/podofo/private/XmlUtils.h
@@ -10,6 +10,7 @@
 #include <functional>
 #include <libxml/tree.h>
 #include <libxml/xmlerror.h>
+#include <libxml/parser.h>
 #include <podofo/auxiliary/nullable.h>
 
 // Cast macro that keep or enforce const to use
