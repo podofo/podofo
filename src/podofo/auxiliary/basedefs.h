podofo/podofo

commit b808094dfb5ac860958b32d9e9f5a069ca9c5432

basedefs: Move sanitization of <Windows.h> defines to all kind of builds
It was mistakenly present only the in PODOFO_SHARED branch

Fixes https://github.com/podofo/podofo/issues/351#issuecomment-4690634113

src/podofo/auxiliary/basedefs.h
@@ -56,6 +56,7 @@
     #define PODOFO_IMPORT __attribute__ ((visibility("default")))
     #define PODOFO_DEPRECATED __attribute__((__deprecated__))
 #endif
+#endif
 
 // If detected, undefine some macros that are defined by Windows
 // headers and that may cause errors when consuming PoDoFo. To
@@ -87,8 +88,6 @@
 #define PODOFO_API PODOFO_EXPORT
 #else
 #define PODOFO_API PODOFO_IMPORT
-#endif
-
 #endif
 
 // Set up some other compiler-specific but not platform-specific macros

commit 32e19743d373f422f4b668cadd85466b4c8c8d5a

basedefs.h: Fixed moving PODOFO_API out of PODOFO_SHARED
Fix a regression in b808094dfb5ac860958b32d9e9f5a069ca9c5432 while trying to fix https://github.com/podofo/podofo/issues/351#issuecomment-4690634113

src/podofo/auxiliary/basedefs.h
@@ -38,6 +38,7 @@
 #define PODOFO_IMPORT
 
 #else // PODOFO_SHARED
+
 #ifndef PODOFO_SHARED
 #define PODOFO_SHARED
 #endif
@@ -56,6 +57,13 @@
     #define PODOFO_IMPORT __attribute__ ((visibility("default")))
     #define PODOFO_DEPRECATED __attribute__((__deprecated__))
 #endif
+
+#if defined(PODOFO_BUILD)
+#define PODOFO_API PODOFO_EXPORT
+#else
+#define PODOFO_API PODOFO_IMPORT
+#endif
+
 #endif
 
 // If detected, undefine some macros that are defined by Windows
@@ -82,12 +90,6 @@
 #ifdef DrawText
 #undef DrawText
 #endif // DrawText
-#endif
-
-#if defined(PODOFO_BUILD)
-#define PODOFO_API PODOFO_EXPORT
-#else
-#define PODOFO_API PODOFO_IMPORT
 #endif
 
 // Set up some other compiler-specific but not platform-specific macros