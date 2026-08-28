0e0cf5a3149639d951518fc552515a8e8e989f3e
Francesco Pretto <ceztko@gmail.com>
2026-08-28 13:10:21 +0200

    charconv_compat: std::to_chars is available in iOS with version >= 16.3
    Should fix https://github.com/podofo/podofo/issues/363

--- src/podofo/private/charconv_compat.h
+++ src/podofo/private/charconv_compat.h
@@ -11,7 +11,7 @@
 
 // Check for Apple targets runtime checks for std::to_chars availability. CHECK-ME: watchOS, tvOS, visionOS???
 // Should fix https://github.com/podofo/podofo/issues/295
-#if defined(__apple_build_version__) && __apple_build_version__ >= 17000000 && ((defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED < 130300) || (defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && __IPHONE_OS_VERSION_MIN_REQUIRED < 130300))
+#if defined(__apple_build_version__) && __apple_build_version__ >= 17000000 && ((defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED < 130300) || (defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && __IPHONE_OS_VERSION_MIN_REQUIRED < 160300))
 // The following avoids including to_chars_floating_point.h
 #define _LIBCPP___CHARCONV_TO_CHARS_FLOATING_POINT_H
 #define TO_CHARS_APPLE_TARGET_TOO_LOW
